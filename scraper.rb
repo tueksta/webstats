aef333b2dadf699f4cfce87e65d3754679f5764a
tueksta <andreas.beer@googlemail.com>
2016-10-17 19:39:30 +0200

    Formatting of tabs

--- scraper.rb
+++ scraper.rb
@@ -7,7 +7,7 @@ require './importsource'
 						:imdb 		 => ImportSource.new('http://www.imdb.com/list/',							'?start=1&view=compact&sort=created:desc&defaults=1',->(arg) { arg.css('#main .article .header .nav .desc')[0]['data-size'] }),
 						:tvmaze 	 => ImportSource.new('http://www.tvmaze.com/users/',					'/stats',																						 ->(arg) { arg.css('section#activity ul li span')[0].text }),
 						:grouvee	 => ImportSource.new('https://www.grouvee.com/user/',					'',																									 ->(arg) { arg.css('.sidebar-nav ul li a')[2].text[8..-2]}),
-					}
+					 }
 
 #seed me as testuser
 sourceIDs = {
@@ -21,8 +21,8 @@ andreas = User.new(name: 'Andreas', sourceIDs: sourceIDs)
 
 #scrape it
 def showCounter (source:, userID:)
-  puts source
-  puts @sources[source].countForUser(userID: userID)
+	puts source
+	puts @sources[source].countForUser(userID: userID)
 end
 
 andreas.listSources.each { |source| showCounter(source: source, userID: andreas.sourceToID(source: source)) }
