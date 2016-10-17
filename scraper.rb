6ff5b1c435505734b464215ece85fd004009ce8d
tueksta <andreas.beer@googlemail.com>
2016-10-17 19:46:11 +0200

    Formatting of tabs

--- scraper.rb
+++ scraper.rb
@@ -3,9 +3,9 @@ require './importsource'
 
 #seed sources
 @sources = {
-            :goodreads => ImportSource.new('https://www.goodreads.com/review/list/','?shelf=read',                                       ->(arg) { arg.css('.h1Shelf span')[0].text[1..-2] }),
-            :imdb      => ImportSource.new('http://www.imdb.com/list/',              '?start=1&view=compact&sort=created:desc&defaults=1',->(arg) { arg.css('#main .article .header .nav .desc')[0]['data-size'] }),
-            :tvmaze    => ImportSource.new('http://www.tvmaze.com/users/',          '/stats',                                             ->(arg) { arg.css('section#activity ul li span')[0].text }),
+            :goodreads => ImportSource.new('https://www.goodreads.com/review/list/', '?shelf=read',                                        ->(arg) { arg.css('.h1Shelf span')[0].text[1..-2] }),
+            :imdb      => ImportSource.new('http://www.imdb.com/list/',              '?start=1&view=compact&sort=created:desc&defaults=1', ->(arg) { arg.css('#main .article .header .nav .desc')[0]['data-size'] }),
+            :tvmaze    => ImportSource.new('http://www.tvmaze.com/users/',           '/stats',                                             ->(arg) { arg.css('section#activity ul li span')[0].text }),
             :grouvee   => ImportSource.new('https://www.grouvee.com/user/',          '',                                                   ->(arg) { arg.css('.sidebar-nav ul li a')[2].text[8..-2]}),
            }
 
